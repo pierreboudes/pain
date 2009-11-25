70dc3347c588be905afb2f52f73eeb6ab4fd4e25
pierreboudes <boudes@univ-paris13.fr>
2009-11-25 22:47:42 +0000

    : infobox version de prod
    git-svn-id: https://www-lipn.univ-paris13.fr/svn/licences/trunk/outils/pain@343 04a62a05-9fe5-4f9b-8506-f2d01d604607

--- sources/inc_infobox.php
+++ sources/inc_infobox.php
@@ -25,7 +25,10 @@ $toto = 0 + 0;
 Bonjour <?=$_SERVER['PHP_AUTH_USER']?>.
 <p>      
       En cas de problème d'affichage à l'édition :<b> recharger la page.</b></p>
-
+      <p>Jusqu'à janvier 2010 au moins, il faut également <b>tenir 
+le <a href="http://www-lipn.univ-paris13.fr/~boudes/wikka/wikka.php?wakka=NoticeServices2009">wiki</a> 
+à jour. </b>
+</p>
       <p>La  <a href="https://www-lipn.univ-paris13.fr/projects/licences/wiki/AccueilPain" title="Accueil pain">page web de pain</a> contient des informations complémentaires. Un système de tickets, pour signaler des bugs ou demander des améliorations, y est disponible (vérifier que le bug n'est pas déjà signalé dans un ticket avant de créer un nouveau ticket).</p>
 </div>
 </center>
