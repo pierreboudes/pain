890d654aeebb0cd3063811aecac0d7c40896de91
boudes <boudes@lipn.univ-paris13.fr>
2015-09-17 13:36:19 +0200

    bugfix rapporté par Pascal Boyer

--- enseignants.php
+++ enseignants.php
@@ -19,7 +19,6 @@
  * You should have received a copy of the GNU General Public License
  * along with Pain.  If not, see <http://www.gnu.org/licenses/>.
  */
-require_once("inc_connect.php");
 require_once('authentication.php'); 
 $user = authentication();
 $annee = get_and_set_annee_menu();
