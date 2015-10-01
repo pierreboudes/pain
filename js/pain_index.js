5797070141e274894a303cbf5d5bc399b4f49804
boudes <boudes@lipn.univ-paris13.fr>
2015-10-01 22:47:35 +0200

    on peut pas pousser les murs

--- js/pain_index.js
+++ js/pain_index.js
@@ -24,7 +24,7 @@
 $(document).ready(function(){
 	/* masqer certaines colonnes et les squelettes de lignes */
 	$('#skelcours').children('th.section, th.credits, th.mcc, th.debut, th.fin, th.tirage, th.inscrits, th.presents, th.totaux_loader, th.collections, th.tags').fadeOut(0); // th.alt, th.inscrits, th.presents,
-//	$('#skeltranche').children('th.declarer').fadeOut(0);
+	$('#skeltranche').children('th.nature').fadeOut(0);
 	$('#skelchoix').children('th.cm, th.td, th.tp, th.alt, th.prp, th.referentiel, th.choix').fadeOut(0);
 	$('#skellongchoix').children('th.cm, th.td, th.tp, th.alt, th.prp, th.referentiel, th.choix, th.semestre').fadeOut(0);
 	$('#skel').fadeOut(0);
